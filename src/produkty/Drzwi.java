d1230a38074ce63d0c337523a07e836037657724
Przemek Hawdziejuk <przemek.hawdziejuk@gmail.com>
2015-01-22 09:35:03 +0100

    Merge branch 'master' of https://github.com/stampedplague/sklep
    Conflicts:
    	nbproject/private/private.properties
    	nbproject/project.properties
    	src/sklep/okna.java

---
+++ src/produkty/Drzwi.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package produkty;
+
+/**
+ *
+ * @author Senyama
+ */
+public class Drzwi {
+
+    
+    public Drzwi()
+    {
+        System.out.println("[produkty.Drzwi] Utworzono obiekt klasy Drzwi");
+    }
+}
