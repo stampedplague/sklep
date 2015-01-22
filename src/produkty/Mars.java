a8e3a5e1a2ad69bb1adabf6277f5296b62e21bc2
Przemek Hawdziejuk <przemek.hawdziejuk@gmail.com>
2015-01-22 09:45:09 +0100

    Merge branch 'master' of https://github.com/stampedplague/sklep
    Conflicts:
    	nbproject/private/private.properties
    	nbproject/project.properties
    	src/sklep/okna.java

---
+++ src/produkty/Mars.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package produkty;
+
+/**
+ *
+ * @author Senyama
+ */
+public class Mars {
+    
+    public Mars()
+    {
+        System.out.println("[produkty.Mars] Utworzono obiekt klasy Mars");
+    }
+}
