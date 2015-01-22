b0f03cbe4af77b071abf1cd9650772067f33364f
Przemek Hawdziejuk <przemek.hawdziejuk@gmail.com>
2015-01-22 09:37:21 +0100

    Merge branch 'master' of https://github.com/stampedplague/sklep
    Conflicts:
    	nbproject/private/private.properties
    	nbproject/project.properties
    	src/sklep/okna.java

---
+++ src/produkty/Uszczelki.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package produkty;
+
+/**
+ *
+ * @author Senyama
+ */
+public class Uszczelki {
+    
+    public Uszczelki()
+    {
+        System.out.println("[produkty.Uszczelki] Utworzono obiekt klasy Uszczelki");
+    }
+}
